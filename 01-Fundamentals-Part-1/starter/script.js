e7ed1f6abb20f3bad2c0618284449cb945dcfa3c
Jasmin Köhler <jasminangelique@gmail.com>
2025-08-19 12:54:40 +0200

    variables and values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,24 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is fun");
-
+// if (js === "amazing") alert("Javascript is fun");
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jaz")
+console.log(23)
+
+// Variables and Values
+let firstName = "Jaz";
+console.log(firstName);
+
+let jaz_coco = "JC";
+let $function = 27;
+
+let person = "Jaz";
+let PI = 3.1415;
+
+let myFirstJob = "Painter";
+let myCurrentJob = "Programmer";
+
+let job1 = "Painter";
+let job2 = "Programmer";
+
+console.log(myFirstJob);
