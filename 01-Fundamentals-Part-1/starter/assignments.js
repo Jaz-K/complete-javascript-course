311568dcfb56a5e993bf799fee740f604421329f
Jasmin Köhler <jasminangelique@gmail.com>
2025-08-20 18:23:57 +0200

    basic operators + assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -17,3 +17,14 @@ console.log(typeof language);
 language = "german"
 // isIsland = "true" 
 // not possiple to change a const valua
+
+console.log("population", population / 2);
+population++
+
+console.log(population > 6);
+console.log(population < 33);
+
+const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+console.log(description);
+
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -62,4 +62,35 @@ job = "content creater";
 catsName = "Coco" //Don't do this always declare a variable
 console.log(catsName); */
 
+// Basic Operators
+// Math Operators
+const currentYear = 2037
+const ageJaz = currentYear - 1991;
+const ageRoelant = currentYear - 2018;
+
+console.log(ageJaz, ageRoelant);
+console.log(ageJaz * 2, ageJaz / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Jaz"
+const lastName = "K."
+
+console.log(firstName + " " + lastName);
+//Concatinating strings
+
+//Assignmenr  Operators
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+console.log(x);
+
+//Comparison Operators  (<,>,=>,=<)
+console.log(ageJaz > ageRoelant); //greater than
+console.log(ageRoelant >= 18); // greater or equal
+
+const isFullAge = ageRoelant >= 18;
+
+console.log(currentYear - 1991 > currentYear  - 2018);
 
