da80a52a6c6c07fddb737f17182776d80148816a
Jasmin Köhler <jasminangelique@gmail.com>
2025-09-18 11:41:50 +0200

    functions calling functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -67,3 +67,18 @@ const yearUntilRetirement = (birthYear, firstName) => {
 console.log(yearUntilRetirement(1985, "Coco"));
 console.log(yearUntilRetirement(1980, "Bob"));
  */
+
+//FUNCTIONS CALLING FUNCTIONS
+
+function cutFruitPieces(fruit) {
+    return fruit * 4
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples)
+    const orangePieces = cutFruitPieces(oranges)
+    const juice = `juice with ${applePieces} piece of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
