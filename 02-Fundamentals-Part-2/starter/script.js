cefe2bcafd1a4a3764f0998e57987e0e82f25bb7
Jasmin Köhler <jasminangelique@gmail.com>
2025-09-18 13:59:21 +0200

    array introduction

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -106,3 +106,54 @@ const yearUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearUntilRetirement(1985, "Coco"));
 console.log(yearUntilRetirement(1970, "Mike")); */
+
+// ARRAYS
+
+const friend1 = "Coco";
+const friend2 = "Rocky";
+const friend3 = "Elliot";
+
+const friends = ["Coco", "Rocky", "Elliot"];
+
+console.log(friends);
+
+const y = new Array(1984, 1999, 2005, 2018);
+console.log(y);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length -1]);
+
+friends[2] = "Jay"
+console.log(friends);
+// friends = ["Bob", "Jerry"]
+
+const firstName = "Ellias";
+const Ellias = [
+    firstName,
+    "Ainthworth",
+    2037 - 1993,
+    "Magican",
+    ["Coco", "Rocky", "Elliot"],
+];
+
+console.log(Ellias);
+
+
+// Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1985, 2010, 2018, 1979, 1954];
+
+console.log(calcAge(years)); // this does not work NaN
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length-1]);
+console.log(age1, age2,age3)
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length-1])];
+console.log(ages);
