19dcc7e25bee73ad309e2337de8d1cf9a2eacd28
Jasmin Köhler <jasminangelique@gmail.com>
2025-10-06 12:33:01 +0200

    first dom manipulations

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const message = (document.querySelector('.message').textContent =
+  '🎉 Correct Number');
+const score = (document.querySelector('.score').textContent = 15);
+const number = (document.querySelector('.number').textContent = 10);
+const input = (document.querySelector('.guess').value = 50);
+
+console.log(message);
+console.log(document.querySelector('.guess').value);
