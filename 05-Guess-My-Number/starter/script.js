84527bddb409eb13360a6b1d8b234d820934211e
Jasmin Köhler <jasminangelique@gmail.com>
2025-10-06 14:44:53 +0200

    guessing game part 2 dom manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,43 +12,49 @@ console.log(document.querySelector('.guess').value); */
 const button = document.querySelector('.check');
 const input = document.querySelector('.guess');
 const message = document.querySelector('.message');
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
 const number = document.querySelector('.number');
 const shownScore = document.querySelector('.score');
 const resetButton = document.querySelector('.again');
+const body = document.querySelector('body');
+const highscore = document.querySelector('.highscore');
 
-let score = 5;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 shownScore.textContent = score;
 console.log('score', score);
 
 button.addEventListener('click', sendNumber);
-resetButton.addEventListener('click', resetGame);
 
 function sendNumber() {
   const guess = Number(input.value);
-  console.log('guess', guess, typeof guess);
 
+  //Whene there is no input
   if (!guess) {
     message.textContent = '🛑 No Number';
+
+    //When player wins
   } else if (guess === secretNumber) {
     number.textContent = secretNumber;
     message.textContent = '🎉 Correct Number';
+    body.style.backgroundColor = '#60b347';
+    number.style.width = '30rem';
+
+    //When guess is to high
   } else if (guess > secretNumber) {
     if (score > 1) {
       message.textContent = '📈 too high';
       score--;
       shownScore.textContent = score;
-      console.log('score', score);
     } else {
       message.textContent = '💥 You lost the game"';
       shownScore.textContent = 0;
     }
+    //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       message.textContent = '📉 too low';
       score--;
       shownScore.textContent = score;
-      console.log('score', score);
     } else {
       message.textContent = '💥 You lost the game"';
       shownScore.textContent = 0;
@@ -56,8 +62,24 @@ function sendNumber() {
   }
 }
 
+// Reset game by refreshing page
+/* resetButton.addEventListener('click', refreshPage);
+
+function refreshPage() {
+  window.location.reload();
+} */
+
+resetButton.addEventListener('click', resetGame);
+
 function resetGame() {
-  console.log('lets reset the game');
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  message.textContent = 'Star guessing...';
+  shownScore.textContent = score;
+  input.value = '';
+  number.textContent = '?';
+  number.style.width = '15rem';
+  body.style.backgroundColor = '#222';
 }
 
 console.log('secretNumber', secretNumber);
