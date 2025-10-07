2596dc15d6a7874e9c17af8bba909a658b19156f
Jasmin Köhler <jasminangelique@gmail.com>
2025-10-07 10:56:50 +0200

    project 2 modal adding functionality with classList and key down function

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,6 +6,25 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log(btnsShowModal);
 
-for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
 // NO {} needed because its a one liner
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
