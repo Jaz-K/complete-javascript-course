5f6c17651ad11c782831a63ed4cba825a05f82fa
Jasmin Köhler <jasminangelique@gmail.com>
2025-10-07 11:44:27 +0200

    project 3 pig game adding dice roll funtionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -8,13 +8,33 @@ const currentScoreP0 = document.querySelector('#current--0');
 const currentScoreP1 = document.querySelector('#current--1');
 const dice = document.querySelector('.dice');
 
-const rollDice = document.querySelector('.btn--roll');
-const holdPoints = document.querySelector('.btn--hold');
-const newGame = document.querySelector('.btn--new');
+const btnNewGame = document.querySelector('.btn--new');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnHoldPoints = document.querySelector('.btn--hold');
 
 //starting condition
 scoreP0.textContent = 0;
 scoreP1.textContent = 0;
 dice.classList.add('hidden');
 
+let currentScore = 0;
+
 //ROLL DICE
+
+btnRollDice.addEventListener('click', rollDice);
+
+function rollDice() {
+  const roll = Math.trunc(Math.random() * 6) + 1;
+
+  dice.classList.remove('hidden');
+  dice.src = `dice-${roll}.png`;
+
+  //check for rolled 1
+  if (roll !== 1) {
+    //add to current score
+    currentScore += roll;
+    currentScoreP0.textContent = currentScore; // CHANGE LATER
+  } else {
+    //switch  to player 2
+  }
+}
