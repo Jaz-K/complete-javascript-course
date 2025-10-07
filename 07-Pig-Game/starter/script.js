794f7cd0971fb7bf97015dff0da608102e9568ee
Jasmin Köhler <jasminangelique@gmail.com>
2025-10-07 13:23:51 +0200

    projec 3 pig game finishing game adding init function and restart button

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,25 +4,40 @@ const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
 const scoreP0 = document.querySelector('#score--0');
 const scoreP1 = document.querySelector('#score--1');
-// const currentScoreP0 = document.querySelector('#current--0');
-// const currentScoreP1 = document.querySelector('#current--1');
+const currentScoreP0 = document.querySelector('#current--0');
+const currentScoreP1 = document.querySelector('#current--1');
 const dice = document.querySelector('.dice');
 
 const btnNewGame = document.querySelector('.btn--new');
 const btnRollDice = document.querySelector('.btn--roll');
 const btnHoldPoints = document.querySelector('.btn--hold');
 
-//starting condition
-scoreP0.textContent = 0;
-scoreP1.textContent = 0;
-dice.classList.add('hidden');
+let scores = [],
+  currentScore,
+  activePlayer,
+  playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  //starting condition
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  scoreP0.textContent = 0;
+  scoreP1.textContent = 0;
+  currentScoreP0.textContent = 0;
+  currentScoreP1.textContent = 0;
+
+  dice.classList.add('hidden');
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+};
+init();
+
 //ROLL DICE
-
 btnRollDice.addEventListener('click', rollDice);
 
 function rollDice() {
@@ -62,7 +77,7 @@ btnHoldPoints.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     //check score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish game
       playing = false;
       document
@@ -78,3 +93,29 @@ btnHoldPoints.addEventListener('click', function () {
     }
   }
 });
+
+btnNewGame.addEventListener('click', init);
+
+//my version
+
+/* btnNewGame.addEventListener('click', function () {
+  //set playing to active
+  playing = true;
+  // remove winning css
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  //reset scores current scores
+  currentScore = 0;
+  scoreP0.textContent = 0;
+  scoreP1.textContent = 0;
+  currentScoreP0.textContent = 0;
+  currentScoreP1.textContent = 0;
+  //reset scores array
+  scores[0] = 0;
+  scores[1] = 0;
+  //set back to player one as starting player
+  activePlayer = 0;
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+}); */
