2ebe2f3875f79ae0b4a19b536c1222d3b16b60ad
Jasmin Köhler <jasminangelique@gmail.com>
2025-10-14 13:48:41 +0200

    Hoisting

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-function calcAge(birthYear) {
+// SCOPE AND SCOPECHAIN
+/* function calcAge(birthYear) {
   const age = 2034 - birthYear;
 
   function printAge() {
@@ -35,3 +36,49 @@ const firstName = 'Jaz';
 calcAge(1985);
 // console.log(age);
 // printAge();
+ */
+
+// HOISTING
+//Variables
+console.log('me', me);
+// console.log('job', job);
+// console.log('year', year);
+
+var me = 'Jaz';
+let job = 'Programmer';
+const year = 1985;
+
+//functions
+
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+//EXAMPLE
+console.log(numProducts); //numProducts is undefined so its falsy
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('all products deleted');
+}
+
+var x = 1; // var creates property in the global window object
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
