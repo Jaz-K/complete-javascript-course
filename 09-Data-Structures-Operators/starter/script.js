8436ce0dbdf5eb9f74d9926f83ad538a5b38ff2f
Jasmin Köhler <jasminangelique@gmail.com>
2025-10-24 10:58:46 +0200

    optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -30,7 +30,7 @@ const openingHours = {
     open: 12,
     close: 22,
   },
-  weekdays[4]: {
+  [weekdays[4]]: {
     open: 11,
     close: 23,
   },
@@ -437,3 +437,33 @@ for (const [i, el] of menu.entries()) {
 // [0, 'Focaccia'] .entries() creates arrays with the index and the item
 
 console.log(...menu.entries());
+
+//OPTIONAL CHAINING
+// without
+// console.log(restaurant.openingHours.mon.open); //error message
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+//with optional chaining
+console.log(restaurant.openingHours.mon?.open); // undefiend
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`on ${day} we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+//Arrays
+const users = [{ name: 'Coco', isMayor: true, favFood: 'Fish' }];
+
+console.log(users[0]?.name ?? 'User array empty'); // Coco
+console.log(users[1]?.name ?? 'User array empty'); // 'User array empty'
+
+// if (users.length > 0) console.log(users[0]?.name);
+// else console.log('User array empty');
