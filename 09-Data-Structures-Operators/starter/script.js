0cf25961a8bc2447728c620c1da4feb250b8427a
Jasmin Köhler <jasminangelique@gmail.com>
2025-10-29 12:30:41 +0100

    Map iteration  and conversion

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -615,32 +615,68 @@ console.log(italianFoods.isDisjointFrom(mexicanFoods)); */
 
 // MAPS
 
-const rest = new Map();
+// const rest = new Map();
 
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Rom, Italy');
-rest.set(2, 'Hamburg, Germany');
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 22)
-  .set(true, 'We are open')
-  .set(false, 'We are closed');
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Rom, Italy');
+// rest.set(2, 'Hamburg, Germany');
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 22)
+//   .set(true, 'We are open')
+//   .set(false, 'We are closed');
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get(1));
-console.log(rest);
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+// console.log(rest);
 
-const time = 21;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
-console.log(rest.has('categories'));
-rest.delete(2);
-const arr = [1, 2];
-rest.set(arr, 'test');
-rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest);
-// rest.clear();
-console.log(rest.size);
-console.log(rest.get(arr));
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// const arr = [1, 2];
+// rest.set(arr, 'test');
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// // rest.clear();
+// console.log(rest.size);
+// console.log(rest.get(arr));
+
+// MAPS CREATION AND ITERATION
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavScript'],
+  ['correct', 3],
+  [true, 'Correct 🥳'],
+  [false, 'Try again'],
+]);
+console.log(question);
+
+//convert object to map
+console.log(Object.entries(openingHours));
+const hourMap = new Map(Object.entries(openingHours));
+console.log(hourMap);
+
+//quiz app
+console.log(question.get('question'));
+const answer = 3;
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Anser ${key}: ${value}`);
+  }
+}
+// const answer = Number(prompt('Your answer'));
+console.log(answer);
+console.log(question.get(question.get('correct') === answer));
+
+// convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
