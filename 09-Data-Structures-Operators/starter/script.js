f4b714a85382d715396320ae518a4feb790ba27a
Jasmin Köhler <jasminangelique@gmail.com>
2025-10-29 14:42:19 +0100

    Football Challange #3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -647,7 +647,7 @@ console.log(italianFoods.isDisjointFrom(mexicanFoods)); */
 
 // MAPS CREATION AND ITERATION
 
-const question = new Map([
+/* const question = new Map([
   ['question', 'What is the best programming language in the world?'],
   [1, 'C'],
   [2, 'Java'],
@@ -680,3 +680,59 @@ console.log([...question]);
 // console.log(question.entries());
 console.log([...question.keys()]);
 console.log([...question.values()]);
+ */
+
+/* Coding Challenge #3
+Let's continue with our football betting app! This time, we have a map called
+'gameEvents' (see below) with a log of the events that happened during the
+game. The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17:
+⚽
+GOAL
+GOOD LUCK 😀 */
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 3.1
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 3.2
+gameEvents.delete(64);
+// 3.3
+const time = [...gameEvents.keys()].pop();
+console.log(`An event happened, on
+average, every ${time / gameEvents.size} minutes.`);
+
+console.log(time);
+// 3.4
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'First' : 'Second';
+  console.log(`[${half} Half] ${min}: ${event}`);
+  // console.log(
+  //   key <= 45
+  //     ? `[First Half] ${min}: ${event}`
+  //     : `[Second Half] ${min}: ${event}`
+  // );
+}
